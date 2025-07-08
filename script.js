858b78ccabc0fc50153214200f5dfd5c2fd5b80f
qasagava <qasagava@yandex.ru>
2025-07-08 20:23:28 +0300

    v1.5

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function characterBuilder() {
   return {
     assets: {
       'Background': [
-        'https://e1719e2a-268b-49a4-868c-99eb98ec37df.srvstatic.uz/Background/1%20(2).png',
+        'https://raw.githubusercontent.com/qasagava/Haties/refs/heads/main/1%20(2).png',
       ],
       'Back asset': [
         'https://e1719e2a-268b-49a4-868c-99eb98ec37df.srvstatic.uz/Back%20asset/1%20(1).png',
@@ -55,32 +55,55 @@ function characterBuilder() {
       this.activeCategory = this.activeCategory === layer ? null : layer;
     },
     select(layer, url) {
-      if (layer === 'Hair') this.selected['Asset'] = null;
-      if (layer === 'Asset') this.selected['Hair'] = null;
-      this.selected[layer] = url;
+       if (layer === 'Hair') this.selected['Asset'] = null;
+  if (layer === 'Asset') this.selected['Hair'] = null;
+
+  this.selected[layer] = url;
+
+  anime({
+    targets: `#layer-${this.slugify(layer)}`,
+    opacity: [0, 1],
+    scale: [0.9, 1],
+    duration: 600,
+    easing: 'easeOutExpo'
+  });
     },
     randomize() {
-      for (let layer in this.assets) {
-        if (['Background', 'Back asset', 'Base'].includes(layer)) continue;
-        if (this.assets[layer].length > 0) {
-          this.selected[layer] = Math.random() > 0.5
-            ? this.assets[layer][Math.floor(Math.random() * this.assets[layer].length)]
-            : null;
-        }
-      }
+  for (let layer of this.layerOrder) {
+    if (['Background', 'Back asset', 'Base'].includes(layer)) continue;
 
-      if (this.selected['Hair'] && this.selected['Asset']) {
-        if (Math.random() > 0.5) this.selected['Hair'] = null;
-        else this.selected['Asset'] = null;
-      }
-    },
-    downloadImage() {
-      html2canvas(document.getElementById('preview')).then(canvas => {
-        const link = document.createElement('a');
-        link.download = 'character.png';
-        link.href = canvas.toDataURL();
-        link.click();
+    const options = this.assets[layer];
+    this.selected[layer] = Math.random() > 0.5 && options.length > 0
+      ? options[Math.floor(Math.random() * options.length)]
+      : null;
+  }
+
+  if (this.selected['Hair'] && this.selected['Asset']) {
+    if (Math.random() > 0.5) this.selected['Hair'] = null;
+    else this.selected['Asset'] = null;
+  }
+
+  anime({
+    targets: '.preview img',
+    opacity: [0, 1],
+    duration: 700,
+    easing: 'easeOutCubic'
+  });
+},
+
+ downloadImage() {
+  const node = document.getElementById('preview');
+  html2canvas(node, {
+    backgroundColor: null, // Чтобы не было белого фона
+    useCORS: true,         // Важно для изображений с других доменов!
+    scale: 2               // Лучше качество PNG
+  }).then(canvas => {
+    const link = document.createElement('a');
+    link.download = 'character.png';
+    link.href = canvas.toDataURL('image/png');
+    link.click();
       });
     }
   };
 }
+
